642a8c6eee0f15245ed66183040e97578dc85925
WisDomz <houseofwem1@gmail.com>
2023-12-04 20:41:01 +0100

    readme Authors basemodel console

---
+++ console.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb)"
+    def do_EOF(self, line):
+        return True
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()

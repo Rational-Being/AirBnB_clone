b5ef8b0caffe56f4965a4e110ecd935d4297a6f0
Rational-Being <overcomer021@gmail.com>
2023-12-05 07:10:06 -0800

    the print reload in the test file of task 5 doesnt work i am not sure the reload method itself is efficent enough i implemented storage in basemodel.py almost done with task 5

---
+++ models/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from models.engine.file_storage import FileStorage
+
+"""
+creating a new instnce form the class FileStorage in the filesrogae.py
+"""
+storage = FileStorage()
+"""
+called the public instanc emethod form filestorage on the new instance
+"""
+storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -2,50 +2,54 @@
 
 import uuid
 from datetime import datetime
+from models import storage
 
-class BaseModel():
+class BaseModel:
     """
     This class defines all common attributes for other class
     """
+
     def __init__(self, *args, **kwargs):
         """This constructor takes the *args and **kwargs argument
         *args is not to be used
         """
         if len(args) > 0:
             pass
-        
+
         if kwargs is not None and kwargs != dict():
             for key, value in kwargs.items():
                 if key in ["created_at", "updted_at"]:
-                    self.__dict__[key] = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__[key] = datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f"
+                    )
                 elif key == "id":
                     self.id = value
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """
         This method is called when an object is to be orinted
         """
         return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
-    
+
     def save(self):
         """
         This updates the public instance updted_At
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
         A serialiazation/deserialization process that returns a dictionary
         containing all key/values of __dict__ of the instance
         """
-        inst_dict = self.__dict__.copy()     #instance dictionary is stored here
+        inst_dict = self.__dict__.copy()  # instance dictionary is stored here
         inst_dict["__class__"] = type(self).__name__
         inst_dict["created_at"] = inst_dict["created_at"].isoformat()
         inst_dict["updated_at"] = inst_dict["updated_at"].isoformat()
         return inst_dict
-
-
